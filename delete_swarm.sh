8f005c9858cb3e487fda7b1ef0cbcbea85c102da
buhtigexa <mrodriguez@alumnos.exa.unicen.edu.ar>
2017-03-22 12:52:38 -0300

    Frena y borra los nodos del cluster

--- delete_swarm.sh
+++ delete_swarm.sh
@@ -1,14 +1,17 @@
  
  #!/bin/bash
  
+echo " Frenado  y eliminando máquinas virtuales ...."
 
  docker-machine stop worker1
  docker-machine stop worker2
  docker-machine stop manager
 
+
  docker-machine rm -y worker1
  docker-machine rm -y worker2
  docker-machine rm -y manager
+
 
 sudo docker stop $(docker ps -aq) && docker rm $(docker ps -aq)
 sudo docker ps
